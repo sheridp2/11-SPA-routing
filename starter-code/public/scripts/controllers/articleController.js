904ede5be7cdf732d3e47601f771f4550682c645
Patrick Sheridan <sheridp2@gmail.com>
2017-03-28 16:53:36 -0700

    4 TODOs done, home page now shows based on url

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -2,10 +2,13 @@
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  articleController.init = function(){
+    $('.tab-content').hide();
+    $('#articles').fadeIn();
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-
+  }
 
   module.articleController = articleController;
 })(window);
--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,6 +1,12 @@
 'use strict';
 
+page('/', articleController.init);
+// page('/about',);
+// page('/new', );
+// page('/articles', );
+page();
+
 // TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+// DONE: What function do you call to activate page.js? Fire it off now, to execute
