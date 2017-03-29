550449c36e9c00888ee6536e77650a28531b62cd
Benjamin <ayzenbergb@gmail.com>
2017-03-28 17:08:15 -0700

    finished all todo's

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -2,8 +2,11 @@
 
 (function(module) {
   const aboutController = {};
-
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.init = function() {
+    $('.tab-content').hide();
+    $('#about').fadeIn();
+  }
+  // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
 
 
   module.aboutController = aboutController;
